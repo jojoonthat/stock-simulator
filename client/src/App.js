52bb8d4700b7260dc5a0ee51d67f7afe22f8debf
Joanne Pan <jpan0917@gmail.com>
2022-06-10 06:57:48 +0100

    Moved polling logic into `PriceDisplay`

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,12 @@
-import { useState, useEffect } from "react"
 import Slider from "./components/Slider";
 import PriceDisplay from "./components/PriceDisplay";
 import './App.css';
 
 function App() {
-  const [currPrice, setCurrPrice] = useState(null)
-
-  useEffect(() => {
-    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
-
-    const refresh = async () => {
-      while (true) {
-        const { curr_price } = await (await fetch("http://localhost:5000/next-tick")).json()
-        setCurrPrice(curr_price);
-        await sleep(1000)
-      }
-    }
-
-    refresh()
-  }, [])
-
   return (
     <div id="app">
-      {currPrice &&
-        <>
-          <PriceDisplay price={currPrice}/>
-          <Slider />
-        </>
-      }
+      <PriceDisplay />
+      <Slider />
     </div>
   );
 }
--- client/src/components/PriceDisplay.jsx
+++ client/src/components/PriceDisplay.jsx
@@ -1,6 +1,24 @@
-function PriceDisplay({ price }) {
+import { useState, useEffect } from "react";
+
+function PriceDisplay() {
+  const [currPrice, setCurrPrice] = useState(null)
+
+  useEffect(() => {
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+    const refresh = async () => {
+      while (true) {
+        const { curr_price } = await (await fetch("http://localhost:5000/next-tick")).json()
+        setCurrPrice(curr_price);
+        await sleep(1000)
+      }
+    }
+
+    refresh()
+  }, [])
+
   return (
-    <p className="price-display">${price.toFixed(5)}</p>
+    <p className="price-display">${currPrice?.toFixed(5)}</p>
   )
 }
 
