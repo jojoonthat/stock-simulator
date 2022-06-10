55329daceec0d7330109d18a1011bc27f23431b4
Joanne Pan <jpan0917@gmail.com>
2022-06-10 06:50:24 +0100

    Slider

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
+
 function Slider() {
+  const [sliderVal, setSliderVal] = useState(5000);
+
+  const handleChange = e => {
+    setSliderVal(e.target.value)
+  }
+
   return (
-    <div>1</div>
+    <div className="slider-container">
+      <p>😨</p>
+      <div className="slider">
+        <input type="range" min={0} max={10000} value={sliderVal} onChange={handleChange} />
+        <p>{sliderVal / 10000}</p>
+      </div>
+      <p>🤗</p>
+    </div>
   )
 }
 
