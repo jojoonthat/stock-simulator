3e3ab347adacb4737c903e2b669cab1a3f2081cf
Joanne Pan <jpan0917@gmail.com>
2022-06-10 06:29:14 +0100

    Flask cors allow all

--- server/src/server.py
+++ server/src/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import logging
 from Simulation.Simulation import Simulation
 
@@ -8,6 +9,7 @@ log.disabled = True
 
 # Init flask app + simulation instances
 app = Flask(__name__)
+CORS(app)
 simulation = Simulation()
 
 # Return price at simulation's next tick on GET /next-tick
